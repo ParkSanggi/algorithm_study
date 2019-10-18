03b760858eb5365b1126355815891c23979e2c7f
ParkSanggi <roqkfwkehlwk@naver.com>
2019-10-18 19:28:40 +0900

    fix missing last number

--- build_from_base_case.py
+++ build_from_base_case.py
@@ -3,7 +3,7 @@ from copy import copy
 # 문자열의 모든 순열을 계산하는 알고리즘을 설계하라
 # 모든 문자는 문자열 내에서 고유하게 등장한다고 가정
 
-string = ['a', 'b', 'c', 'd']
+string = ['0', '1', '2']
 
 
 def find_permutation(_list):
@@ -22,7 +22,9 @@ def find_permutation(_list):
             base_for_combination = copy(element)
             base_for_combination.insert(j, last)
             combined_elements.append(base_for_combination)
-    return base_elements + combined_elements
+
+    return base_elements + combined_elements + [[last]]
 
 
-find_permutation(string)
+for i in find_permutation(string):
+    print(i)
